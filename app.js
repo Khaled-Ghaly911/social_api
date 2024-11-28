ba28e857687deece78f47751ba4dea0a7e011dce
Khaled Ghaly <khaledghaly000@gmail.com>
2024-11-28 20:50:48 +0200

    err handling & connect to database

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
+const path = require('path')
+
 const express = require('express');
 
 const bodyParser = require('body-parser');
 
+const mongoose = require('mongoose');
+
 const feedRoutes = require('./routes/feed');
+const { Result } = require('express-validator');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use('images/', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -25,4 +31,16 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    res.status(status).json({
+        message: message
+    });
+})
+
+mongoose.connect('mongodb+srv://khaledghaly000:eH0xSez5dM09Ar2I@cluster0.s5xgw.mongodb.net/messages?retryWrites=true&w=majority&appName=Cluster0')
+.then(result => {
+    app.listen(8080);
+}).catch(err => console.log(err))
--- controller/feed.js
+++ controller/feed.js
@@ -1,3 +1,6 @@
+const { validationResult } = require('express-validator')
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
       posts: [
@@ -15,20 +18,36 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.createPost = (req, res, next) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        const error = new Error('validation failed, entered data is not correct');
+        error.statisCode = 422;
+        throw error
+    }
     const title = req.body.title;
     const content = req.body.content;
     //create post in db 
-
-    res.status(201).json({
-        message: 'post created successfully!',
-        post: {
-            _id: new Date().toISOString(), 
-            title: title, 
-            content: content,
-            creator: {
-                name: 'Khaled Ghaly'
-            },
-            createdAt: new Date()
-        }
+    const post = new Post({
+        title: title, 
+        content: content,
+        imageUrl: 'images/home.jpg',
+        creator: {
+            name: 'Khaled Ghaly'
+        },
     })
+    post.save()
+    .then(result => {
+        console.log(result);
+        res.status(201).json({
+            message: 'post created successfully!',
+            post: result
+        });    
+    })
+    .catch(err => {
+        if(!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err)
+    })
+    
 }
